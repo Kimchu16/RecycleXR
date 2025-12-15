d06e8d00f8277777e038dc6cadea7d6abb875739
Kimchu16 <kimberlylibranza@gmail.com>
2025-12-15 19:54:47 +0000

    Add material icons to trash items
    Introduced new material label icons (FSC, Glass, PET) and updated item scenes to display these icons as Sprite3D nodes. Modified trash.gd to support a label_icon export and show/hide the icon when the item is grabbed or released.

--- scripts/trash.gd
+++ scripts/trash.gd
@@ -5,11 +5,28 @@ signal sorted_correct
 enum MaterialType {
 	PLASTIC,
 	PAPER,
-	GLASS
+	GLASS,
+	CARD
 }
 
 @export var material_type : MaterialType
+@export var label_icon : Texture2D
 
+@onready var icon_sprite : Sprite3D = $Label_Icon
+
+func _ready():
+	super._ready() # Run parent ready function
+	icon_sprite.texture = label_icon
+	icon_sprite.visible = false   # hidden until grabbed
+	
+	grabbed.connect(_on_grabbed)
+	released.connect(_on_released)
+
+func _on_grabbed(pickable, by):
+	icon_sprite.visible = true
+
+func _on_released(pickable, by):
+	icon_sprite.visible = false
 
 func on_sorted_correctly():
 	emit_signal("sorted_correct")
