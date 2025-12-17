466f442257e3a98f0df3329c91cad6ed55b28dfc
Kimchu16 <kimberlylibranza@gmail.com>
2025-12-17 02:01:59 +0000

    Add button press sound effect to push button
    Added a metallic button press audio file and integrated it with the push button. Updated the scene to include an AudioStreamPlayer3D node and modified the script to play the sound when the button is pressed. Also adjusted Label3D positions and pixel sizes for improved scoreboard appearance.

--- scripts/push_button.gd
+++ scripts/push_button.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var button := $InteractableAreaButton
+@onready var audio := $AudioStreamPlayer3D
 
 @export var scoreboard_manager : Node
 
@@ -8,4 +9,5 @@ func _ready():
 	button.button_pressed.connect(_on_button_pressed)
 
 func _on_button_pressed(button):
+	audio.play()
 	scoreboard_manager.reset()
